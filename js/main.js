ebee1d80c95aaa295655ff6044c544535b08cbb3
sb227 <69783498+sb227@users.noreply.github.com>
2020-10-06 18:01:37 +0900

    201006

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
 /// 메인 페이지 JS - main.js ////
 
-$(function () { /////// jQB ///////////////////////
-    console.log("로딩완료!");
+var hel = Array('h', 'e', 'l', 'v', 'e', 't', 'c', 'a');
+
+$(function (e) { /////// jQB ///////////////////////
+console.log("로딩완료!");
+
+// 사이드 메뉴
+$(".menu").click(function () {
+    $(this).toggleClass("on");
+    $("header").toggleClass("on");
+    $(".gnb_list").toggleClass("on");
+})
+
+// 원 날리기~
+
+//Add random divs as sample....
+//Change numberOfDivs if you want
+var numberOfDivs = 80;
+
+// add x number of divs to page.
+for (var i = 0; i < numberOfDivs; i++) {
+    var g = Math.floor(Math.random() * 5);
+    var c = Math.floor(Math.random() * 5);
+    //Create div with class c0/c3 (color) and g0/g4 (size)
+    var p = $('<div/>').addClass('c' + c).addClass('g' + g).addClass('parti');
+    //Add object to body
+    $('#cont1').append(p);
+}
+
+
+//This is where the magic happends!!!
+//Add css set to none because I added display:block;position:absolute; to the css.
+$('.parti').movingBubble({
+    addCss: false
+});
 
 
 }); ////////// jQB ///////////////////////////////
